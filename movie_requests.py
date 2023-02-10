c5a505c88488fa61a72ed3dfbf16dcf3d8df8e53
Darth Vader <danieltarancon@gmail.com>
2023-02-10 19:28:31 +0100

    Update movie_requests.py

--- movie_requests.py
+++ movie_requests.py
@@ -6,34 +6,23 @@ from movieapi import api_key
 with open('oscar_winners.csv', 'r') as file:
     data = file.read()
 
-#print(data.split('\n')[1].split(',')[1])
-
-print(data.split('\n'))
-
-data_list = data.split('\n')
-data_list.pop(0)
 
 
-movies = {}
-
-for movie in data_list:
-    movies[movie.split(',')[0]]=movie.split(',')[1]
-
-print(movies)
+def cleaner(data):
+    data_list = data.split('\n')
+    data_list.pop(0)
+    movies = {}
+    for movie in data_list:
+        movies[movie.split(',')[0]] = movie.split(',')[1]
+    return movies
 
 
 
-
-
-
-
-
-def request(parameter):
+def request(id):
     response = ''
-    url = 'http://www.omdbapi.com/?' + parameter + '&apikey=' + api_key
+    url = 'http://www.omdbapi.com/?' + 'i=' + id + '&apikey=' + api_key
     response = requests.get(url)
     print(response.json())
-
 
 
 
