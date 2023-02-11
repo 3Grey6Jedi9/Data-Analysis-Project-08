4b51e07cae53792fde285f3189b120403cd5582e
Darth Vader <danieltarancon@gmail.com>
2023-02-11 13:51:46 +0100

    Update prk.py

--- prk.py
+++ prk.py
@@ -4,4 +4,15 @@ url = 'http://www.omdbapi.com/?i=tt5580390&apikey=2da3c928'
 
 response = requests.get(url)
 
-print(response.json())
+#print(response.json())
+
+
+
+u = '45,333,888'
+
+w = u.split(',')
+
+v = ''
+
+print(int(w[2])*10)
+
