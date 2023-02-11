74ec19129db73143f645204ea1fe4e500dd8b1a6
Darth Vader <danieltarancon@gmail.com>
2023-02-11 20:40:32 +0100

    Update movie_requests.py

--- movie_requests.py
+++ movie_requests.py
@@ -1,4 +1,4 @@
-import requests, csv
+import requests, csv, pdb
 
 from movieapi import api_key
 
@@ -37,7 +37,7 @@ a = cleaner(data)
 
 new_data = request(a['Parasite'])
 
-print(new_data)
+#print(new_data)
 
 def clean_movie(data):
     data_keys = []
@@ -63,18 +63,25 @@ def clean_movie(data):
     award_nominations_index = award_winds_index + 1
     box_office_index = data_keys.index('Box Office')
     j = 0
+    #pdb.set_trace()
     for value in data_values:
         if j == runtime_index:
-            pass
+            data_values[j] = int(value.split(' ')[0])
+            j += 1
         elif j == award_winds_index:
-            pass
-        elif j == award_nominations_index:
-            pass
+            data_values[j] = int(value.split(' ')[3])
+            data_values.insert(j + 1, int(value.split(' ')[6]))
+            j += 1
         elif j == box_office_index:
-            pass
+            figure = value.split('$')[1]
+            fig_parts = figure.split(',')
+            data_values[j] = int(fig_parts[2]) + int(fig_parts[1]) * 1000 + int(fig_parts[0]) * 1000000
+            j += 1
         else:
             j += 1
             continue
+    print(data_values)
+
 
 
 clean_movie(new_data)
