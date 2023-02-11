051b9213e73c50ed0274a119a74ee711860d2e35
Darth Vader <danieltarancon@gmail.com>
2023-02-11 13:51:43 +0100

    Update movie_requests.py

--- movie_requests.py
+++ movie_requests.py
@@ -37,15 +37,28 @@ a = cleaner(data)
 
 print(request(a['Parasite']))
 
-def clean_movie(data):
-    for k, v in data.items():
-        if k == 'Runtime':
-            data[k]=int(v.split(' ')[0])
-        elif k == ''
-        else:
-            continue
-            
+#Separete the dictionary into two different lists then clean the data and put the data into a CSV file
 
+#def clean_movie(data):
+    #for k, v in data.items():
+        #if k == 'Runtime':
+            #data[k]=int(v.split(' ')[0])
+        #elif k == 'Awards':
+            #data[k] = int(v.split(' ')[3])
+            #k = 'Award Wins'
+            #data['Award Nominations'] = int(v.split(' ')[6])
+        #elif k == 'BoxOffice':
+            #a = v.split('$')[1]
+            #b = a.split(',')
+            #data[k] = int(b[2]) + int(b[1])*1000 + int(b[0])*1000000
+            #k = 'Box Office'
+        #else:
+            #continue
+    #return data
+
+#print(clean_movie(request(a['Parasite'])))
+
+# Do it using a list
 
 
 
