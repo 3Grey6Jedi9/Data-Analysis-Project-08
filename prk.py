c92038c5355f7da58ef1b9a6ee50b0a41d7e12fe
Darth Vader <danieltarancon@gmail.com>
2023-02-12 23:20:47 +0100

    Update prk.py

--- prk.py
+++ prk.py
@@ -1,42 +1,11 @@
-import requests
-
-url = 'http://www.omdbapi.com/?i=tt5580390&apikey=2da3c928'
-
-response = requests.get(url)
-
-#print(response.json())
 
 
+a = {'b':90, 'c': 30}
 
-u = '45,333,888'
+print(2 in range(1,len(a)+1))
 
-w = u.split(',')
+print(len(a))
 
-v = ''
-
-print(int(w[2])*10)
-
-
-L = ['a', 'b', 1,2]
-
-L[0] = 'C'
-
-print(L)
-
-if j == runtime_index:
-    # data_values[j] = int(value.split(' ')[0])
-    #print('It is working')
-#elif j == award_winds_index:
-    #data_values[j] = int(value.split(' ')[3])
-    data_values.insert(j + 1, int(value.split(' ')[6]))
-elif j == box_office_index:
-    figure = value.split('$')[1]
-    fig_parts = figure.split(',')
-    data_values[j] = int(fig_parts[2]) + int(fig_parts[1]) * 1000 + int(fig_parts[0]) * 1000000
-else:
-    j += 1
-    continue
-print(data_values)
 
 
 
